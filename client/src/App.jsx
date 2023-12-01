ccdbd178507c512db271b1d6044266e06f512047
AGorelski <132019892+AGorelski@users.noreply.github.com>
2023-12-01 21:05:39 +0200

    Create game service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import GameCreate from "../components/create-game/GameCreate"
-import Details from "../components/details/Details"
-import GameList from "../components/game-list/GameList"
-import Header from "../components/header/Header"
-import Home from "../components/home/Home"
-import Login from "../components/login/Login"
-import Register from "../components/register/Register"
+import GameCreate from "./components/create-game/GameCreate"
+import Details from "./components/details/Details"
+import GameList from "./components/game-list/GameList"
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Login from "./components/login/Login"
+import Register from "./components/register/Register"
 
 import { Route, Routes } from "react-router-dom"
 
