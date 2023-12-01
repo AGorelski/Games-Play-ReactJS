404345b25effb576b33b0e42a08af65ed822a1e3
AGorelski <132019892+AGorelski@users.noreply.github.com>
2023-12-01 20:36:21 +0200

    added routing to all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
+import GameCreate from "../components/create-game/GameCreate"
+import Details from "../components/details/Details"
+import GameList from "../components/game-list/GameList"
 import Header from "../components/header/Header"
+import Home from "../components/home/Home"
+import Login from "../components/login/Login"
+import Register from "../components/register/Register"
+
+import { Route, Routes } from "react-router-dom"
+
 
 function App() {
 
   return (
     <div id="box">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create-game" element={<GameCreate />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+
     </div>
   )
 }
