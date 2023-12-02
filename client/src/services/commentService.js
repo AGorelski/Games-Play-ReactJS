f823690fd649ab1c99be4b7f67b91ac3ca81fb41
AGorelski <132019892+AGorelski@users.noreply.github.com>
2023-12-02 23:16:07 +0200

    Show comments for each user

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,9 +3,12 @@ import * as request from "../lib/request";
 const baseURL = "http://localhost:3030/jsonstore/games/comments"
 
 export const getAll = async (gameId) => {
-    const result = await request.get(baseURL);
+    const query = new URLSearchParams({
+        where: `gameId: "${gameId}"`
+    }) 
+    const result = await request.get(`${baseURL}`);
 
-    return Object.values(result);
+    return Object.values(result).filter(comment => comment.gameId === gameId);
 }
 
 export const create = async (gameId, username, text) => {
